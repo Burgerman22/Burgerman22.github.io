6c156e08cffa9e873332abcdcb8c901570b67526
Louis Donaldson <louis.donaldson42@gmail.com>
2024-11-20 15:13:38 +0000

    Animation added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
   //   AddDividers();
+
+  setTimeout(() => {
+    const loading_div = document.querySelector(".loading");
+    loading_div.classList.add("fade");
+  }, 2000);
 });
